e4ee95b286d45f0db46a4c39fd920574c9aa86ed
GabrielSantana013 <gabrielsantanadias133@gmail.com>
2026-03-31 20:34:12 -0300

    feat: Number

--- src/analisadorLexico/AFD.java
+++ src/analisadorLexico/AFD.java
@@ -1,4 +1,27 @@
 package analisadorLexico;
 
-public class AFD {
+import java.text.CharacterIterator;
+
+public abstract class AFD {
+
+    protected LexTable lexTable;
+
+    public AFD(LexTable lexTable){
+        this.lexTable = lexTable;
+    }
+
+    public abstract Token evaluate(CharacterIterator code);
+
+    public boolean isTokenSeparator(CharacterIterator code){
+        return code.current() == ' ' ||
+                code.current() == '+' ||
+                code.current() == '-' ||
+                code.current() == '*' ||
+                code.current() == '/' ||
+                code.current() == '(' ||
+                code.current() == ')' ||
+                code.current() == '\n' ||
+                code.current() == CharacterIterator.DONE;
+    }
+
 }
--- src/analisadorLexico/Number.java
+++ src/analisadorLexico/Number.java
@@ -1,4 +1,48 @@
 package analisadorLexico;
 
-public class Number {
+import java.text.CharacterIterator;
+
+public class Number extends AFD{
+
+    public Number(LexTable lexTable) {
+        super(lexTable);
+    }
+
+    @Override
+    public Token evaluate(CharacterIterator code) {
+
+        if(Character.isDigit(code.current())) {
+            StringBuilder number = new StringBuilder();
+            boolean isFloat = false;
+            number.append(readNumber(code));
+
+            if(code.current() == '.'){
+                isFloat = true;
+                number.append('.');
+                code.next();
+
+                if(!Character.isDigit(code.current())) {
+                    //tratar erro futuro
+                    return null;
+                }
+                number.append(readNumber(code));
+            }
+            if(isTokenSeparator(code)){
+                String lexema = number.toString();
+                InputTable ref = lexTable.add(lexema, 0);
+                String type = isFloat ? "NUM_FLOAT" : "NUM_INT";
+                return new Token(type, ref);
+            }
+        }
+        return null;
+    }
+
+    private String readNumber(CharacterIterator code) {
+        StringBuilder number = new StringBuilder();
+        while(Character.isDigit(code.current())) {
+            number.append(code.current());
+            code.next();
+        }
+        return number.toString();
+    }
 }
