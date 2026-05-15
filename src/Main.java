9b18372742fa6f9afee323967d949c0d66013eaf
PedroSchneider1 <162492569+PedroSchneider1@users.noreply.github.com>
2026-05-15 01:32:57 -0300

    fix: parametro de diretorio de saida

--- src/Main.java
+++ src/Main.java
@@ -3,38 +3,39 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
-
 import analisadorLexico.Lexer;
 import analisadorLexico.Token;
 import analisadorSintatico.Parser;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-
-        // flags
         boolean printTokens = Arrays.asList(args).contains("-t");
         boolean printArvore = Arrays.asList(args).contains("-a");
+        String code = Files.readString(Path.of(args[0]));
 
-        String code = Files.readString(Path.of("codigo.txt"));
+        String outputPath = null;
+        for (int i = 0; i < args.length - 1; i++) {
+            if (args[i].equals("-o")) {
+                outputPath = args[i + 1];
+                break;
+            }
+        }
 
         Lexer lexer = new Lexer(code);
         List<Token> tokens = lexer.getTokens();
 
-        if(printTokens){
+        if (printTokens) {
             System.out.println("===== TOKENS =====");
-            for(Token token : tokens){
+            for (Token token : tokens) {
                 System.out.println(token);
             }
             System.out.println("==================\n");
         }
 
         Parser parser = new Parser(tokens, printArvore);
-        if(parser.main()){
+        if (parser.main()) {
+	    parser.salvarArquivo(outputPath);
             parser.sucess();
-            parser.salvarArquivo("saida.rs");
-        } else {
-            parser.error("Main");
         }
     }
 }
